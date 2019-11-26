1a56b0f9e7a44327a355b58c4d24abc789b9fdb5
Ravi Jha <ravishawnjha@Ravis-MacBook-Pro-2.local>
2019-11-26 10:27:28 -0600

    manifest json an background script

---
+++ addon/background-script.js
@@ -0,0 +1,6 @@
+
+setTimeout(() => {
+  console.log(document.querySelectorAll('iframe').src)
+  document.querySelectorAll('iframe').src = "https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg"
+}, 1000)
+  document.getElementById('iframe').src = "https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg"
