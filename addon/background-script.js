c67234e89d93a6fa0dba716c0d0f51f0cbac01e0
Ravi Jha <ravishawnjha@Ravis-MacBook-Pro-2.local>
2019-12-05 19:19:23 -0600

    background.js edited

--- addon/background-script.js
+++ addon/background-script.js
@@ -1,4 +1,9 @@
 console.log(document.querySelectorAll('iframe'))
-setTimeout(() => {
+/* setTimeout(() => {
   document.querySelector('iframe').src = "https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg"
-}, 1000)
+}, 1000) */
+const ad = document.querySelectorAll('iframe');
+
+for
+
+ad[0].setAttribute ('src','https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg')
