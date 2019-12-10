02cf2adee24cd187f84d0ef2fae8851140e6f887
Maria Teresa Gómez Molina <mayte@MacBook-Pro-de-Maria.local>
2019-12-10 11:54:35 -0600

    fixed the trayectory of the button

--- server/www/termsofuse/js/termsofuse.js
+++ server/www/termsofuse/js/termsofuse.js
@@ -2,12 +2,11 @@ const userPreference = document.getElementById('button')
 let count = 0
 
 userPreference.onclick = function () {
-  const x = Math.random() * window.innerWidth
-  const y = Math.random() * window.innerHeight
+  const x = Math.random() * window.innerWidth - 120
+  const y = Math.random() * window.innerHeight - 40
+  userPreference.style.position = 'fixed'
   userPreference.style.top = y + 'px'
-  userPreference.style.bottom = y + 'px'
   userPreference.style.left = x + 'px'
-  userPreference.style.right = x + 'px'
   count++
   if (count > 10) {
     window.location = '../popup.html'
