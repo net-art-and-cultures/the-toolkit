faec386b4ce15148012c30e283b2dfc5dc7b8de6
Emilia Vidal-Hallett <Sk8rGrl@Emilias-MacBook-Pro-2.local>
2019-12-09 18:51:58 -0600

    ٩(^ᴗ^)۶

--- server/www/fountain.js
+++ server/www/fountain.js
@@ -1,14 +1,14 @@
-var of;
+let of;
+let c1, c2;
 const button = document.querySelector('input');
-const paragraph = document.querySelector('p');
-
-
-button.addEventListener('click', updateButton);
-
+const Y_AXIS = 100;
+const X_AXIS = 200;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    var t =
+    c1 = color(204, 102, 0);
+    c2 = color(0, 102, 153);
+    let t =
     {
         name: "test",
         colors: ["red"],
@@ -21,13 +21,38 @@ function setup() {
     of = new Fountain(null, t);
 }
 
-
 function draw() {
-  background(200,200,400);
+  // background(200,200,400);
+  setGradient(0, 0, width / 2, height, c1, c2, X_AXIS);
+  setGradient(width / 2, 0, width / 2, height, c2, c1, X_AXIS);
+
   of.Draw();
   of.CreateN();
   of.Step();
   noStroke();
   text(of.length, width/2, 20);
   stroke(0);
+}
+
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+  if (axis === Y_AXIS) {
+    // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis === X_AXIS) {
+    // Left to right gradient
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y + h);
+    }
+  }
 }
