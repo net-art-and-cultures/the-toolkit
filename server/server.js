af5a5f3535b02da1b21cc1081b5faf05e9e2d242
nbriz <nickbriz@gmail.com>
2019-12-10 07:18:41 -0600

    moved cors to api-images

--- server/server.js
+++ server/server.js
@@ -4,16 +4,10 @@ const getimagesAPI = require('./api-getimages.js')
 const uploadAPI = require('./api-upload.js')
 const port = Number(process.argv[2]) || 8000
 const staticFiles = express.static(`${__dirname}/www`)
+
 app.use(staticFiles)
 app.use(getimagesAPI)
 app.use(uploadAPI)
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  next()
-})
 
 //heres an example API endpoint
 app.get('/poop', (req, res) => {
