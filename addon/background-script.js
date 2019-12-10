485d169bcdaf6ae4fb4b17cb391215b6d2bd394a
Ravi Jha <ravishawnjha@Ravis-MacBook-Pro-2.local>
2019-12-09 18:32:52 -0600

    addon

--- addon/background-script.js
+++ addon/background-script.js
@@ -1,13 +1,13 @@
-let ad = document.querySelectorAll('iframe'){
-const img = document.createElement('img')
+let ad = document.querySelectorAll('iframe')
 
 for (let i = 0; i < ad.length; i++ ){
   let father = ad[i].parentElement
   father.removeChild(ad[i])
 
+const img = document.createElement('img')
+
   for (let z = 0; z < images.length; z++ ){
-      img.src = 'images[z]'
+      img.src = 'https://i.imgur.com/lVlPvCB.gif'
       father.appendChild(img)
     }
-}
-}
+    }
